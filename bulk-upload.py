026a1ba6dc8d6efca80a0f93fa496f5e1627b55b
Henok <hhenok1987@gmail.com>
2022-12-19 15:06:42 +0100

    feat: bulk upload restructure

--- bulk-upload.py
+++ bulk-upload.py
@@ -1,9 +1,14 @@
+import os
+
 import boto3
 from os import walk
-
 from botocore.exceptions import ClientError
+from dotenv import load_dotenv
 
-bucket = 'employee-documents-for-bulk-download-test'
+load_dotenv()
+
+bucket = os.environ.get('AWS_BUCKET')
+bulk_upload_dir = os.environ.get('BULK_UPLOAD_DIR')
 
 def upload(file_name, key):
 	s3 = boto3.client('s3')
@@ -18,10 +23,10 @@ def upload(file_name, key):
 
 
 def start_file_upload():
-	for (_, __, files) in walk('bulk-files'):
+	for (_, __, files) in walk(bulk_upload_dir):
 		for i, file in enumerate(files):
-			print('uploading %s => %s' % (i/len(files), file))
-			upload('bulk-files/'+file, file)
+			print('uploading %s => %s' % ((i+1)/len(files), file))
+			upload(bulk_upload_dir+'/'+file, file)
 
 
 
